923a21a3a635943b4e50a326eaa602ca70d07016
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-18 15:24:55 -0400

    JSDoc format examples

--- js/default-params.js
+++ js/default-params.js
@@ -14,3 +14,35 @@ const getArea = (width, length, unit = 'sq ft') => {
   return `${area} ${unit}`
 }
 //set to whatever customary unit of measure is where the majority of your users are
+
+//Below is JSDoc format (except the functions themselves)
+
+/**
+ * [A short description of the myFunc function]
+ *
+ * @param {[param type]} param1 - [parameter description]
+ * @param {[param type]} param2 - [parameter description]
+ * @returns {[return type]} [documents the function's return value]
+ */
+
+/*
+function myFunc( param1, param2 ) {
+  // function returns a value...
+}
+ */
+
+/**
+ * Calculates and returns the area of a rectangle.
+ *
+ * @param {number} width - The width of the rectangle.
+ * @param {number} length - The length of the rectangle.
+ * @param {string} unit - The unit of measurement.
+ * @returns {string} The area of the rectangle and unit of measure.
+ */
+
+/*
+function getArea(width, length, unit) {
+  const area = width * length;
+  return `${area} ${unit}`;
+}
+  */
